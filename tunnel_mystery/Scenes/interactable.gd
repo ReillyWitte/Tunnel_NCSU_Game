3d7977edd02ec84c3490fc68b4cbcaf90766ffdc
rvwitte <rvwitte@ncsu.edu>
2025-09-26 19:24:37 -0400

    Some dialog stuff added

--- tunnel_mystery/Scenes/interactable.gd
+++ tunnel_mystery/Scenes/interactable.gd
@@ -19,3 +19,8 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 		entered = 0
 		emit_signal("player_exited")
 		print("exit")
+
+func _process(_delta):
+	if entered == 1 and Input.is_action_just_pressed("ui_accept"):
+		if Dialogic.current_timeline == null:
+			Dialogic.start("timeline")
