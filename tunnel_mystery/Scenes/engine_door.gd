3cb8ad83bba2d613d0f8511f96b0d4147bcfd976
Reilly Witte <reillywitte@gmail.com>
2025-09-28 04:05:38 -0400

    Frog game

--- tunnel_mystery/Scenes/engine_door.gd
+++ tunnel_mystery/Scenes/engine_door.gd
@@ -1,5 +1,25 @@
 extends CollisionShape2D
 
+var entered = 0
+
 func _process(delta):
 	if Globals.mole_smoke == 1:
 		shape = null
+
+func _ready():
+	print("Door ready")
+	
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("player"):
+		entered = 1
+		if shape != null:
+			Globals.mole_dialog = 3.0
+			Dialogic.start("mole_timeline")
+		print("entered")
+		
+
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	entered = 0
+	print("exit")
